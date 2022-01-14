62a09cb01fd3adb28727d32aed82cd436dd04470
gcerv93 <gcerv93@protonmail.com>
2022-01-14 15:40:07 -0600

    add recursive fibs

--- lib/recursion.rb
+++ lib/recursion.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 def fibs(num)
   i = 1
   array = [0, 1, 1]
@@ -13,7 +15,23 @@ end
 
 p fibs(5)
 p fibs(6)
-p fibs(7)
-p fibs(8)
-p fibs(9)
 p fibs(10)
+p fibs(13)
+
+def fibs_rec(num, array = [0, 1, 1])
+  return array if num <= 2
+
+  array << fibs_rec(num - 1, array)[-1] + fibs_rec(num - 1)[-2]
+end
+
+# one line for fun
+def fibs_rec2(num, array = [0, 1, 1])
+  num <= 2 ? array : array << fibs_rec(num - 1, array)[-1] + fibs_rec(num - 1)[-2]
+end
+
+p fibs_rec(5)
+p fibs_rec(6)
+p fibs_rec(10)
+p fibs_rec(13)
+
+p fibs_rec2(13)
